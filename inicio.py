9b05965456a1caa0a0c6813692c86b111d4c9459
Romulo Cesar Silvestre Leite <Romulo34006947@edu.df.senac.br>
2024-10-24 20:06:51 -0300

    ok

--- inicio.py
+++ inicio.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask,flash,render_template,request,redirect,url_for
 
 app = Flask(__name__)
+app.secret_key = 'seu_segredo_aqui'  # Necessário para usar flash messages
 
 @app.route('/')
 def index():
@@ -9,6 +10,30 @@ def index():
 @app.route('/despesa', methods=['POST', 'GET'])
 def despesas():
     return render_template('despesa.html')
+
+@app.route('/logar',methods=['POST','GET'])
+def logar():
+    login = request.form['login']
+    senha = request.form['senha']
+    if login == 'romulo' and senha == '123':
+       return f"login:{login} e senha:{senha}"
+    else:
+        return 'sem sucesso'
+
+
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/listadespesas')
+def listar_despesas():
+    return render_template('listadespesas.html')
+
+
+@app.route('/cadastrardesp')
+def cadastrar_despesa():
+    pass
 
 app.run(debug=True) 
 
